379b5edc8aa0bfb75be79094b1c7258729b9b251
Vinicius Costa <viniciuscosta9898@icloud.com>
2025-10-15 19:27:53 -0300

    instance prisma | create users-repository and create im-memmory-users-repository

---
+++ backend/src/repositories/in-memmory/in-memmory-users-repository.ts
@@ -0,0 +1,22 @@
+import { randomUUID } from "node:crypto";
+import { Prisma, User } from "../../../generated/prisma";
+import { UsersRepository } from "../users-repository";
+
+export class InMemmoryUsersRepository implements UsersRepository{
+    public items: User[] = []
+
+    async create(data: Prisma.UserCreateInput){
+        const user = {
+            id: String(randomUUID),
+            name: data.name,
+            email:  data.email,
+            password: data.password,
+            createdAt: new Date(),
+            updatedAt: new Date()
+        }
+
+        this.items.push(user)
+
+        return user
+    }
+}
