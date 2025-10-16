da1d9cb183644f6dd46e03dcd1c41ddc1369cff8
Vinicius Costa <viniciuscosta9898@icloud.com>
2025-10-15 22:25:24 -0300

    create prisma-repository and update models with @@map

---
+++ backend/prisma/migrations/20251016012300_include_map_in_models/migration.sql
@@ -0,0 +1,42 @@
+/*
+  Warnings:
+
+  - You are about to drop the `Task` table. If the table is not empty, all the data it contains will be lost.
+  - You are about to drop the `User` table. If the table is not empty, all the data it contains will be lost.
+
+*/
+-- DropTable
+PRAGMA foreign_keys=off;
+DROP TABLE "Task";
+PRAGMA foreign_keys=on;
+
+-- DropTable
+PRAGMA foreign_keys=off;
+DROP TABLE "User";
+PRAGMA foreign_keys=on;
+
+-- CreateTable
+CREATE TABLE "users" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "name" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
+
+-- CreateTable
+CREATE TABLE "tasks" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "title" TEXT NOT NULL,
+    "description" TEXT NOT NULL,
+    "status" TEXT NOT NULL DEFAULT 'pending',
+    "userId" TEXT,
+    CONSTRAINT "tasks_userId_fkey" FOREIGN KEY ("userId") REFERENCES "users" ("id") ON DELETE SET NULL ON UPDATE CASCADE
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "users_email_key" ON "users"("email");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "tasks_title_key" ON "tasks"("title");
---
+++ backend/src/repositories/prisma/prisma-users-repository.ts
@@ -0,0 +1,41 @@
+import { Prisma } from "../../../generated/prisma";
+import { prisma } from "../../lib/prisma";
+import { UsersRepository } from "../users-repository";
+
+export class PrismaUsersRepository implements UsersRepository{
+    async create(data: Prisma.UserCreateInput) {
+        const user = await prisma.user.create({
+            data
+        })
+
+        return user
+    }
+
+    async findByEmail(email: string) {
+        const user = await prisma.user.findUnique({
+            where:{
+                email
+            }
+        })
+
+        if(!user){
+            return null
+        }
+
+        return user
+    }
+
+    async findById(userId: string) {
+        const user = await prisma.user.findUnique({
+            where:{
+                id: userId
+            }
+        })
+
+        if(!user){
+            return null
+        }
+
+        return user
+    }
+}
