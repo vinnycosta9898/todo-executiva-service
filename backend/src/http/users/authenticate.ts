cd9d8a454ee545e050d8d1f4ea49659961476c69
Vinicius Costa <viniciuscosta9898@icloud.com>
2025-10-15 22:39:09 -0300

    create authenticate controller

---
+++ backend/src/http/users/authenticate.ts
@@ -0,0 +1,62 @@
+import type { UsersRepository } from '../../repositories/users-repository'
+
+import { compare } from 'bcryptjs'
+import { sign } from 'jsonwebtoken'
+
+import { CredentialsInvalidError } from '../../errors/credentials-invalid-error'
+import { PasswordLengthError } from '../../errors/password-length-error'
+import { ResourceNotFoundError } from '../../errors/resource-not-found-error'
+
+interface AuthenticateRequest {
+  email: string
+  password: string
+}
+
+interface AuthenticateResponse {
+  id: string
+  name: string
+  token: string
+}
+
+export class AuthenticateUseCase {
+  constructor(private usersRepository: UsersRepository) {}
+
+  async execute({
+    email,
+    password
+  }: AuthenticateRequest): Promise<AuthenticateResponse> {
+    const user = await this.usersRepository.findByEmail(email)
+
+    if (!user) {
+      throw new ResourceNotFoundError()
+    }
+
+    if (password.length < 8) {
+      throw new PasswordLengthError()
+    }
+
+    const doesPasswordMatch = await compare(password, user.password)
+
+    if (!doesPasswordMatch) {
+      throw new CredentialsInvalidError()
+    }
+
+    const token = sign(
+      {
+        name: user.name,
+        email: user.email
+      },
+      `${process.env.JWT_SECRET}`,
+      {
+        subject: user.id,
+        expiresIn: '1D'
+      }
+    )
+
+    return {
+      id: user.id,
+      name: user.name,
+      token
+    }
+  }
+}
