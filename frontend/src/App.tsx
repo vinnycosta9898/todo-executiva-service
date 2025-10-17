de003df9de07f1fd45cde4c105cd653136be490c
Vinicius Costa <viniciuscosta9898@icloud.com>
2025-10-17 09:03:35 -0300

    add biome and init signup page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,34 +1,63 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="w-screen h-screen bg-black flex ">
+      <div className="w-[40%] flex justify-center items-center">
+        <h1 className="font-bold text-white text-4xl">
+          TODO Service-Executiva
+        </h1>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+
+      <form className="w-auto flex flex-col justify-center items-center gap-8">
+        <div className="flex flex-col">
+          <label htmlFor="" className="font-bold text-white">
+            Nome
+          </label>
+          <input
+            type="text"
+            className="w-[30rem] h-12 bg-white/10 backdrop-blur-xl rounded-md outline-none px-2 text-zinc-400"
+            placeholder="Digite o seu nome"
+          />
+        </div>
+        <div className="flex flex-col">
+          <label htmlFor="" className="font-bold text-white">
+            email
+          </label>
+          <input
+            type="text"
+            className="w-[30rem] h-12 bg-white/10 backdrop-blur-xl rounded-md outline-none px-2 text-zinc-400"
+            placeholder="Digite o seu nome"
+          />
+        </div>
+        <div className="flex flex-col">
+          <label htmlFor="" className="font-bold text-white">
+            Senha
+          </label>
+          <input
+            type="text"
+            className="w-[30rem] h-12 bg-white/10 backdrop-blur-xl rounded-md outline-none px-2 text-zinc-400"
+            placeholder="Digite o seu nome"
+          />
+        </div>
+        <div className="flex flex-col">
+          <label htmlFor="" className="font-bold text-white">
+            Confirme a sua ssenha
+          </label>
+          <input
+            type="text"
+            className="w-[30rem] h-12 bg-white/10 backdrop-blur-xl rounded-md outline-none px-2 text-zinc-400"
+            placeholder="Digite o seu nome"
+          />
+        </div>
+        <button
+          type="submit"
+          className="w-[30rem] h-12 bg-white rounded-md outline-none font-bold text-black"
+        >
+          Cadastre-se Ja
         </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+        {/** biome-ignore lint/a11y/useValidAnchor: <explanation> */}
+        <a href="#">Nao possui um conta? clique aqui</a>
+      </form>
+    </div>
   )
 }
 
